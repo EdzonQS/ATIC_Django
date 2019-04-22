451399425b1aa399fb992ba697962ddac4971d00
edzon.quinteros <edzon.quinteros@gmail.com>
2019-04-22 12:34:27 -0400

    Update production

--- static/js/navburger.cca5ce69972b.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('.navburger').click(function() {
-			$('nav').css("display", "block");
-		});
-	$('#close').click(function() {
-			$('nav').css("display", "none");
-		});
-});
